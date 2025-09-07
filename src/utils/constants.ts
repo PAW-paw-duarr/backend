92ece735667dc27826fa4ba39cd9a275b2c53353
Hilmi <68109788+hilmoo@users.noreply.github.com>
2025-09-07 13:04:40 +0700

    fix (#6)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,11 @@
+import os from "node:os";
+import path from "node:path";
+
 export enum CategoryCapstone {
   Kesehatan = "Kesehatan",
   PengelolaanSampah = "Pengelolaan Sampah",
   SmartCity = "Smart City",
   TransportasiRamahLingkungan = "Transportasi Ramah Lingkungan",
 }
+
+export const TMP_DIR = path.join(os.tmpdir(), "paw-paw");
